21dcffbc0a84c528df584c1f6ecde99c4a5070e4
raphaella-rose <raphaella.rose1@gmail.com>
2021-09-20 17:17:20 +0100

    created quiz functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import React, { useState } from "react";
 import './App.css';
 
 function App() {
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [showScore, setShowScore] = useState(false);
+  const [score, setScore] = useState(0);
+  const questions = [
+    {
+      questionText: `What is the capital of France?`,
+      answerOptions: [
+        {answerText: `New York`, isCorrect: false },
+        {answerText: `London`, isCorrect: false},
+        {answerText: `Paris`, isCorrect: true},
+        {answerText: `Dublin`, isCorrect: false},
+      ],
+    },
+     {
+      questionText: `Who is the CEO of tesla?`,
+      answerOptions: [
+        {answerText: `Jeff Bezoz`, isCorrect: false },
+        {answerText: `Elon Musk`, isCorrect: true},
+        {answerText: `Bill Gates`, isCorrect: false},
+        {answerText: `Tony Stark`, isCorrect: false},
+      ],
+    },
+     {
+      questionText: `Which animated film company created Mickey Mouse?`,
+      answerOptions: [
+        {answerText: `Disney`, isCorrect: true},
+        {answerText: `Pixar`, isCorrect: false},
+        {answerText: `Dreamworks`, isCorrect: false},
+        {answerText: `Blue Sky`, isCorrect: false},
+      ],
+    },
+     {
+      questionText: `How many Harry Potter books are there?`,
+      answerOptions: [
+        {answerText: `1`, isCorrect: false },
+        {answerText: `4`, isCorrect: false},
+        {answerText: `6`, isCorrect: false},
+        {answerText: `7`, isCorrect: true},
+      ],
+    },
+  ]
+  const handleClick = (isCorrect) => {
+  if (isCorrect) {
+   setScore(score + 1);
+  }
+  const nextQuestion = currentQuestion + 1;
+  if (nextQuestion < questions.length) {
+  setCurrentQuestion(nextQuestion);
+  } else {
+  setShowScore(true);
+  }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div className='app'>
+			{showScore ? (
+				<div className='score-section'>You scored {score} out of {questions.length}</div>
+			) : (
+				<>
+					<div className='question-section'>
+						<div className='question-count'>
+							<span>Question {currentQuestion + 1}</span>/{questions.length}
+						</div>
+						<div className='question-text'>{questions[currentQuestion].questionText}</div>
+					</div>
+					<div className='answer-section'>
+					{questions[currentQuestion].answerOptions.map((answerOption, index) => (
+            <button onClick={() => handleClick(answerOption.isCorrect)} key={index}>{answerOption.answerText}</button>
+          ))}	
+					</div>
+				</>
+			)}
+		</div>
+	);
 }
 
 export default App;
